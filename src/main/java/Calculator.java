e724f798e93e9fb0a195bf29875c52d7dcb937ac
Narasimhan N <Narasimhan.N@iiitb.ac.in>
2023-03-21 00:14:51 +0530

    Final Scientific Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,48 +4,77 @@ import org.apache.logging.log4j.Logger;
 
 public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
-    static  int addition(int a,int b){
-        logger.info("In Addition");
-        return a+b;
+    static double findSquareRoot(double a){
+        logger.info("Exec : Square Root Function | Input : "+a);
+        double sqrt = Math.sqrt(a);
+        logger.info("Square Root Calculated : "+sqrt);
+        return sqrt;
     }
-    static int subtraction(int a,int b){
-        logger.info("In Subtraction");
-        logger.info(a);
-        logger.info(b);
+    static double findFactorial(double a){
+        logger.info("Exec : Factorial Function | Input : "+a);
+        double fact=1;
+        for(int i=1;i<=a;i++)
+            fact=fact*i;
+        logger.info("Factorial Computed : "+fact);
+        return fact;
+    }
+    static double findNaturalLog(double a){
+        logger.info("Exec : Natural Log Function | Input : "+a);
+        double natlog=Math.log(a);
+        logger.info("Natural Log Computed : "+natlog);
+        return natlog;
+    }
+    static double findPower(double a,double b){
+        logger.info("Exec : Power Function | Input : "+a+" "+b);
 
-        return a-b;
+        double pow=Math.pow(a,b);
+        logger.info("Power Computed : "+pow);
+        return pow;
     }
 
     public static void main(String[] args) {
         int  choice=1;
-        while(choice!=3){
+        while(choice!=5){
             System.out.println("\n\n\t\t----------------WELCOME TO CALCULATOR ---------------------\n\n");
-            System.out.println("\n Press 1 : for Addition");
-            System.out.println("\n Press 2 : for Subtraction");
-            System.out.println("\n Press 3 : to Exit");
+            System.out.println("\n Press 1 : Square root");
+            System.out.println("\n Press 2 : Factorial");
+            System.out.println("\n Press 3 : Natural Log");
+            System.out.println("\n Press 4 : Power function");
+            System.out.println("\n Press 5 : to Exit");
             System.out.print("\n Your Choice : ");
             Scanner sc = new Scanner(System.in);
             choice = sc.nextInt();
-            int a,b;
+            double a,b;
             switch (choice){
                 case 1:
-                    System.out.println("\n\t******* Welcome to Addition Section *************\n\n");
-                    System.out.println("\n Enter 2 numbers to add");
+                    System.out.println("\n\t******* Welcome to Square root Section *************\n\n");
+                    System.out.print("\n Enter number to find Square Root : ");
                     a = sc.nextInt();
-                    b = sc.nextInt();
-                    System.out.print("\n Addition Result : "+addition(a,b));
+                    System.out.print("\n Square Root of "+a+" = "+findSquareRoot(a));
                     break;
                 case 2:
-                    System.out.println("\n\t******* Welcome to Addition Section *************\n\n");
-                    System.out.println("\n Enter 2 numbers to subtract");
+                    System.out.println("\n\t******* Welcome to Factorial Section *************\n\n");
+                    System.out.print("\n Enter  numbers to find Factorial : ");
                     a = sc.nextInt();
-                    b = sc.nextInt();
-                    System.out.println("\n Addition Result : "+subtraction(a,b));
+                    System.out.println("\n Factorial of "+a+ " = " + findFactorial(a));
                     break;
                 case 3:
-                    System.out.println("\n\t\tBYEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!\n\n");
+                    System.out.println("\n\t******* Welcome to Natural Log Section *************\n\n");
+                    System.out.print("\n Enter number to find Natural Log : ");
+                    double d = sc.nextDouble();
+                    System.out.println("\n Natural Log of "+d+ " = " + findNaturalLog(d));
                     break;
                 case 4:
+                    System.out.println("\n\t******* Welcome to Power Section *************\n\n");
+                    System.out.print("\n Enter the a and b where a^b is tobe calculated : ");
+                    a = sc.nextInt();
+                    b = sc.nextInt();
+                    System.out.println("\n  "+ a+ " to the power " + b+ " = " +findPower(a,b));
+                    break;
+                case 5:
+                    System.out.println("\n\t\tBYEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!\n\n");
+                    break;
+                default:
                     System.out.println("\n\n\t Invalid Inputtttttt!!!!!!\n\n\t\tTry Again :)");
 
             }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,20 +4,20 @@ import org.junit.*;
 public class CalculatorTest {
 
     @Test
-    public void TestAddition1(){
-    Assert.assertEquals(3,Calculator.addition(1,2));
+    public void TestSqrt(){
+    Assert.assertEquals(5.0,Calculator.findSquareRoot(25),.1f);
     }
     @Test
-    public void TestAddition2(){
-        Assert.assertEquals(0,Calculator.addition(121,-121));
+    public void TestFact(){
+        Assert.assertEquals(120.0,Calculator.findFactorial(5),.1f);
     }
     @Test
-    public void TestSubtraction1(){
-        Assert.assertEquals(-1,Calculator.subtraction(1,2));
+    public void TestNatLog(){
+        Assert.assertEquals(1.0,Calculator.findNaturalLog(2.718),0.2f);
     }
     @Test
-    public void TestSubtraction2(){
-        Assert.assertEquals(8,Calculator.subtraction(10,2));
+    public void TestPow(){
+        Assert.assertEquals(8.0,Calculator.findPower(2,3),.1f);
     }
 
 }
