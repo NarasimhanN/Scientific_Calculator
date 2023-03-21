cb236ffbe9cf6e7c58a7e363037c503b2d83c045
Narasimhan N <Narasimhan.N@iiitb.ac.in>
2023-03-21 13:06:28 +0530

    Added functionality

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,37 +1,61 @@
 import java.util.Scanner;
+
+import jdk.jfr.Experimental;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
     static double findSquareRoot(double a){
-        logger.info("Exec : Square Root Function | Input : "+a);
+        logger.info("Exec : Square Root Function ");
         double sqrt = Math.sqrt(a);
-        logger.info("Square Root Calculated : "+sqrt);
+//        logger.info("Square Root Calculated : "+sqrt);
         return sqrt;
     }
     static double findFactorial(double a){
-        logger.info("Exec : Factorial Function | Input : "+a);
+        logger.info("Exec : Factorial Function ");
         double fact=1;
         for(int i=1;i<=a;i++)
             fact=fact*i;
-        logger.info("Factorial Computed : "+fact);
+//        logger.info("Factorial Computed : "+fact);
         return fact;
     }
     static double findNaturalLog(double a){
-        logger.info("Exec : Natural Log Function | Input : "+a);
+        logger.info("Exec : Natural Log Function ");
         double natlog=Math.log(a);
-        logger.info("Natural Log Computed : "+natlog);
+//        logger.info("Natural Log Computed : "+natlog);
         return natlog;
     }
     static double findPower(double a,double b){
-        logger.info("Exec : Power Function | Input : "+a+" "+b);
+        logger.info("Exec : Power Function ");
 
         double pow=Math.pow(a,b);
-        logger.info("Power Computed : "+pow);
+//        logger.info("Power Computed : "+pow);
         return pow;
     }
-
+    static int takeIntInput(){
+        Scanner sc = new Scanner(System.in);
+        try{
+            int inp = sc.nextInt();
+            return inp;
+        }
+        catch (Exception e){
+            System.out.println("\n\t Enter A INTEGER :( not some nonsense ");
+            return -1111111;
+        }
+    }
+    static double takeDoubleInput(){
+        Scanner sc = new Scanner(System.in);
+        try{
+            double inp;
+            inp = sc.nextDouble();
+            return inp;
+        }
+        catch (Exception e){
+            System.out.println("\n\t Enter a DOUBLE/INT , Not some nonsense :(");
+            return -1111111.0;
+        }
+    }
     public static void main(String[] args) {
         int  choice=1;
         while(choice!=5){
@@ -43,39 +67,47 @@ public class Calculator {
             System.out.println("\n Press 5 : to Exit");
             System.out.print("\n Your Choice : ");
             Scanner sc = new Scanner(System.in);
-            choice = sc.nextInt();
+            while((choice=takeIntInput())== -1111111);
             double a,b;
             switch (choice){
                 case 1:
                     System.out.println("\n\t******* Welcome to Square root Section *************\n\n");
                     System.out.print("\n Enter number to find Square Root : ");
-                    a = sc.nextInt();
+                    while((a=takeDoubleInput())== -1111111.0);
+                   // a = sc.nextInt();
                     System.out.print("\n Square Root of "+a+" = "+findSquareRoot(a));
                     break;
                 case 2:
                     System.out.println("\n\t******* Welcome to Factorial Section *************\n\n");
                     System.out.print("\n Enter  numbers to find Factorial : ");
-                    a = sc.nextInt();
+                    while((a=takeDoubleInput())== -1111111.0);
+
+//                    a = sc.nextInt();
                     System.out.println("\n Factorial of "+a+ " = " + findFactorial(a));
                     break;
                 case 3:
                     System.out.println("\n\t******* Welcome to Natural Log Section *************\n\n");
                     System.out.print("\n Enter number to find Natural Log : ");
-                    double d = sc.nextDouble();
-                    System.out.println("\n Natural Log of "+d+ " = " + findNaturalLog(d));
+//                    double d = sc.nextDouble();
+                    while((a=takeDoubleInput())== -1111111.0);
+                    System.out.println("\n Natural Log of "+a+ " = " + findNaturalLog(a));
                     break;
                 case 4:
                     System.out.println("\n\t******* Welcome to Power Section *************\n\n");
                     System.out.print("\n Enter the a and b where a^b is tobe calculated : ");
-                    a = sc.nextInt();
-                    b = sc.nextInt();
-                    System.out.println("\n  "+ a+ " to the power " + b+ " = " +findPower(a,b));
+                    int x,p;
+//                    a = sc.nextInt();
+//                    b = sc.nextInt();
+                    while((x=takeIntInput())== -1111111);
+                    while((p=takeIntInput())== -1111111);
+
+                    System.out.println("\n  "+ x+ " to the power " + p+ " = " +findPower(x,p));
                     break;
                 case 5:
                     System.out.println("\n\t\tBYEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!\n\n");
                     break;
                 default:
-                    System.out.println("\n\n\t Invalid Inputtttttt!!!!!!\n\n\t\tTry Again :)");
+                    System.out.println("\n\n\t Invalid Choice!!!!!!\n\n\t\tTry Again :)");
 
             }
 
